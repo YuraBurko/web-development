27421ce8306b77b4b022635e090ac35d0a0f4c7f
Yura_Burko <yura_burko@mail.ru>
2016-10-11 05:48:35 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-$.ajax({
-    url: "https://formspree.io/yura_burko@mail.ru", 
-    method: "POST",
-    data: {message: },
-    dataType: "json"
+$(function () {
+    $('#send').click(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/yura_burko@mail.ru", 
+            method: "POST",
+            data: {
+                message: $('#name').val(),
+                text: $('#text').val()
+            },
+            dataType: "json"
+        }).done(function() {
+            $('form').html('<h1>Thank you!</h1>')
+        }).fail(function(xhr, err) {
+            $('form').html(xhr.statusText);
+        });        
+    })
 });
